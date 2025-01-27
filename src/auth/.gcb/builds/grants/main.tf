f1b8b2287abe49ed4da9c678b5fe9afc0abfef09
dbolduc <dbolduc@google.com>
2025-01-27 18:36:30 -0500

    ci(auth): terraform for SA integration tests

--- src/auth/.gcb/builds/grants/main.tf
+++ src/auth/.gcb/builds/grants/main.tf
@@ -34,3 +34,71 @@ resource "google_storage_bucket_iam_member" "sa-can-read-build-tarballs" {
 output "runner" {
   value = data.google_service_account.integration-test-runner.id
 }
+
+##########################################
+# Service Account Integration Test Setup #
+##########################################
+
+# TODO(dbolduc) - I don't love the organization (resources / grants / etc.)
+# I think we should split it into:
+# - Common Setup (GCB triggers, links, integration-test-runner, cloud-build-bucket)
+# - service_account creds
+# - authorized_user creds
+# - etc. for new types (e.g. SA impersonation, EA azure, EA aws) (If possible)
+
+# The service account we will use as our principal for testing service account credentials.
+resource "google_service_account" "test-sa-creds-principal" {
+  account_id = "test-sa-creds"
+  display_name = "A service account for testing service account credentials"
+}
+
+# Generate a key for the test service account credentials.
+resource "google_service_account_key" "test-sa-creds-principal-key" {
+  service_account_id = google_service_account.test-sa-creds-principal.name
+}
+
+# This secret stores the ADC json for the principal testing service account credentials.
+resource "google_secret_manager_secret" "test-sa-creds-json-secret" {
+  secret_id   = "test-sa-creds-json"
+  replication {
+    auto {}
+  }
+}
+
+# Store the test service account key in secret manager.
+resource "google_secret_manager_secret_version" "test-sa-creds-json-secret-version" {
+  secret      = google_secret_manager_secret.test-sa-creds-json-secret.id
+  secret_data = base64decode(google_service_account_key.test-sa-creds-principal-key.private_key)
+}
+
+# The "secret" that will be accessed by the principal testing service account
+# credentials.
+#
+# Note that this is not really a "secret", in that we are not trying to hide its
+# contents.
+#
+# In order to validate our credential types, we need a GCP resource we can set
+# fine-grained ACL on. We have picked Secret Manager secrets for this purpose.
+#
+resource "google_secret_manager_secret" "test-sa-creds-secret" {
+  secret_id = "test-sa-creds-secret"
+  replication {
+    auto {}
+  }
+}
+
+# Add a value to the secret.
+resource "google_secret_manager_secret_version" "test-sa-creds-secret-version" {
+  secret      = google_secret_manager_secret.test-sa-creds-secret.id
+
+  # We do not care that the value is public. We are just testing ACLs.
+  secret_data = "service_account"
+}
+
+# Set up secret permissions for service account credentials.
+resource "google_secret_manager_secret_iam_member" "test-creds-sa-secret-member" {
+  project = "${var.project}"
+  secret_id = google_secret_manager_secret.test-sa-creds-secret.id
+  role = "roles/secretmanager.secretAccessor"
+  member = "serviceAccount:${resource.google_service_account.test-sa-creds-principal.email}"
+}
