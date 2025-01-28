5e40b2a8b44df1e3994947d350f4eb1fdac6fc99
dbolduc <dbolduc@google.com>
2025-01-28 13:46:55 -0500

    import SAs

--- src/auth/.gcb/builds/grants/main.tf
+++ src/auth/.gcb/builds/grants/main.tf
@@ -46,15 +46,14 @@ output "runner" {
 # - authorized_user creds
 # - etc. for new types (e.g. SA impersonation, EA azure, EA aws) (If possible)
 
-# The service account we will use as our principal for testing service account credentials.
-resource "google_service_account" "test-sa-creds-principal" {
+# The service account we use as our principal for testing service account credentials.
+data "google_service_account" "test-sa-creds-principal" {
   account_id = "test-sa-creds"
-  display_name = "A service account for testing service account credentials"
 }
 
 # Generate a key for the test service account credentials.
 resource "google_service_account_key" "test-sa-creds-principal-key" {
-  service_account_id = google_service_account.test-sa-creds-principal.name
+  service_account_id = data.google_service_account.test-sa-creds-principal.name
 }
 
 # This secret stores the ADC json for the principal testing service account credentials.
@@ -72,11 +71,11 @@ resource "google_secret_manager_secret_version" "test-sa-creds-json-secret-versi
 }
 
 # The integration test runner needs access to the service account key secret
-resource "google_secret_manager_secret_iam_member" "test-creds-sa-secret-member" {
+resource "google_secret_manager_secret_iam_member" "test-sa-creds-json-secret-member" {
   project = "${var.project}"
   secret_id = google_secret_manager_secret.test-sa-creds-json-secret.id
   role = "roles/secretmanager.secretAccessor"
-  member = "serviceAccount:${resource.google_service_account.integration-test-runner.email}"
+  member = "serviceAccount:${data.google_service_account.integration-test-runner.email}"
 }
 
 # The "secret" that will be accessed by the principal testing service account
@@ -104,9 +103,9 @@ resource "google_secret_manager_secret_version" "test-sa-creds-secret-version" {
 }
 
 # Set up secret permissions for service account credentials.
-resource "google_secret_manager_secret_iam_member" "test-creds-sa-secret-member" {
+resource "google_secret_manager_secret_iam_member" "test-sa-creds-secret-member" {
   project = "${var.project}"
   secret_id = google_secret_manager_secret.test-sa-creds-secret.id
   role = "roles/secretmanager.secretAccessor"
-  member = "serviceAccount:${resource.google_service_account.test-sa-creds-principal.email}"
+  member = "serviceAccount:${data.google_service_account.test-sa-creds-principal.email}"
 }
